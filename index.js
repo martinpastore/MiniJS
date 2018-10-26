c5f03354c6e761302b91f363d27344a89a78d193
martinpastore <martinpablopastore@gmail.com>
2018-10-26 12:58:23 -0300

    improve code

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const component = require('js/components');
+const component = require('./js/components');
 
-exports.render = function(component) {
-    component.readComponent(component);
+exports.render = function(comp) {
+    component.readComponent(comp);
+};
+
+exports.module = function (modules) {
+    for (let i in modules) {
+        component.readComponent(modules[i]);
+    }
+    component.closeIndex();
 }
--- js/components.js
+++ js/components.js
@@ -1,25 +1,62 @@
+let document = '';
+var fs = require('fs');
+
 exports.render = function(component) {
+    if (document === '') document = this.createIndex();
     const html = component.html + component.js + component.css;
-    document.getElementById('burgerjs-app').innerHTML += html;
+    document += html;
 };
 
 exports.readComponent = function (component) {
     const result = {
-        html: component[0],
-        js: component[1],
-        css: component[2]
+        html: this.readFile(component[0].html, 'html'),
+        js: this.compileJS(component[1].js),
+        css: this.compileCSS(component[2].css)
     };
-
-    result.css = this.compileCSS(result.css);
-    result.js = this.compileJS(result.js);
 
     this.render(result);
 };
 
 exports.compileCSS = function(css) {
-    return `<style>${css}</style>`
+    const script = this.readFile(css, 'css');
+    return `<style>${script}</style>`
 };
 
 exports.compileJS = function(js) {
-    return `<script>${js}</script>`
+    const script = this.readFile(js, 'js');
+    return `<script>${script}</script>`
 };
+
+exports.createIndex = function() {
+    document = `<html>
+        <head></head>
+        <body id="burgerjs-app">`;
+    return document;
+};
+
+exports.closeIndex = function() {
+    document += `</body>
+    </html>`;
+    this.build();
+};
+
+exports.readFile = function(fileName, format) {
+    try {
+        var data = fs.readFileSync(`./components/${fileName}/${fileName}.${format}`, 'utf8');
+        return data.toString();
+    } catch(e) {
+        console.log('Error:', e.stack);
+    }
+};
+
+exports.build = function() {
+    const dir = './dist';
+
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+
+    fs.writeFile('./dist/index.html', document, function (err) {
+        if (err) return console.log(err);
+    });
+}
